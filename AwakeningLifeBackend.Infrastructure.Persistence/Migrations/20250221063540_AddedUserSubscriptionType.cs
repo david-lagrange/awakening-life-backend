12f6cc169e2507a82cff60b745ee69d6f7fb3d19
david-lagrange <davidnlagrange@hotmail.com>
2025-02-21 00:36:01 -0600

    Add SubscriptionType and TwoFactorEnabled to AspNetUsers
    This commit introduces two new columns to the `AspNetUsers` table: `SubscriptionType` (integer) with a default value of `0` and `TwoFactorEnabled` (boolean) set to `false` for existing users.
    
    A new migration class `AddedUserSubscriptionType` is created to manage these changes, including methods for applying and reverting the migration. The `RepositoryContextModelSnapshot.cs` file is updated to reflect the new properties in the user entity model, and the migration designer file is also modified to ensure the Entity Framework Core context recognizes the updated table structure.

---
+++ AwakeningLifeBackend.Infrastructure.Persistence/Migrations/20250221063540_AddedUserSubscriptionType.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AwakeningLifeBackend.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserSubscriptionType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SubscriptionType",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "c78cca8c-efa3-4524-ae21-98904dadf303",
+                column: "SubscriptionType",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubscriptionType",
+                table: "AspNetUsers");
+        }
+    }
+}
